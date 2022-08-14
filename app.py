0250185d08c2789ec81b3e303110dd09e940a47c
fahadsidd107 <fsiddiqui107@gmail.com>
2022-08-15 00:02:49 +0500

    work

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from email.policy import default
-from turtle import title
-from flask import Flask , render_template
+from flask import Flask , render_template, request ,redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -20,20 +18,45 @@ class Todo(db.Model):
         
 
 
-@app.route("/")
+@app.route("/" , methods=['GET' , 'POST'])
 def hello_world():
-    todo = Todo(title='Hello', des='World')
-    db.session.add(todo)
-    db.session.commit()
+    if request.method == 'POST':
+        title=request.form['title']
+        des=request.form['des']
+        todo = Todo(title=title, des=des)
+        db.session.add(todo)
+        db.session.commit()
     all_todo = Todo.query.all()
     return render_template("index.html" , all_todo = all_todo)
-    # return "<h1>Hello, World!</h1>"
+    
 
-@app.route("/show")
-def products():
-    all_todo = Todo.query.all()
-    print(all_todo)
-    return "<h1>Products</h1>"
+# @app.route("/show")
+# def products():
+#     all_todo = Todo.query.all()
+#     print(all_todo)
+#     return "<h1>Products</h1>"
+
+@app.route("/update/<int:sno>",methods=['GET' , 'POST'])
+def update(sno):
+    if request.method == 'POST':
+        title=request.form['title']
+        des=request.form['des']
+        update_todo = Todo.query.filter_by(sno=sno).first()
+        update_todo.title=title
+        update_todo.des=des
+        db.session.add(update_todo)
+        db.session.commit()
+        return redirect("/")
+
+    update_todo = Todo.query.filter_by(sno=sno).first()
+    return render_template("update.html" , update_todo = update_todo)
+
+@app.route("/delete/<int:sno>")
+def delete(sno):
+    del_todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(del_todo)
+    db.session.commit()
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
